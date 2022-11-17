499f47f87d6a24e5be701febb510ff55754f0ab7
Alex <gitallex@mail.ru>
2022-11-17 14:41:53 +0200

    ad calculator

--- calculator.py
+++ calculator.py
@@ -1,1 +1,21 @@
-print("heyy")
+# Calculator
+
+what = input("Ce facem? (+ ; - ; * ; /): ")
+
+a = float(input("Introdu primul numar: "))
+b = float(input("Introdu al doilea numar: "))
+
+if what == "+":
+    c = a + b
+    print("Rezultat: " + str(c))
+elif what == "-":
+    c = a - b
+    print("Rezultat: " + str(c))
+elif what == "*":
+    c = a * b
+    print("Rezultat: " + str(c))
+elif what == "/":
+    c = a / b
+    print("Rezultat: " + str(c))
+else:
+    print("Ati facut ceva gresit...")
--- main.py
+++ main.py
@@ -1,1 +1,49 @@
-print("Hello world")
+# # int (integer - de la numere intregi)
+# number = 5
+#
+# # float (numere zecimale)
+# fnumber = 5.7
+#
+# # str (tot ce se i-a intre " " si ' '
+# name = "Alex"
+#
+# age = 20
+#
+# #bool determina True sau False
+# status = True
+#
+# #Avem doua functii pentru inceput: print() si input(), in (Se introduc argumentele)
+# print("Ce va aparea pe ecran? ") #sau
+# print(name)
+#
+# #putem combina functiile intr-un singur print cu ajutorul "+"
+# print(" 'Numele' meu este " + name)
+#
+# print("Eu ma numesc " + name + ", eu am " + str(age) + " de ani!")
+
+#----------------------------------------------------------------------------
+
+# name = input("Introduceti prenumele dvs: ")
+# age = input("Introduceti varsta dvs: ")
+#
+# print("Salut " + name + ", ai deja  " + str(age) + " de ani!, felicitari :)")
+
+# +, -, /, *
+
+# a = 5
+# b = 7
+#
+# c = a + b
+#
+# print(c)
+
+#----------------------------------------------------------------------
+
+#functiile round - rotungeste, math.floor - rotungeste in partea mai mica,
+# math.ceil - rotungeste in partea mai mare.
+
+import math
+
+a = 5.56
+
+print(round(a))
