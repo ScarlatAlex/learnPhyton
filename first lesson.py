438da43e245c735916f5641a563714f4e301e279
Alex <gitallex@mail.ru>
2022-11-23 22:39:13 +0200

    ad some new

--- first lesson.py
+++ first lesson.py
@@ -6,3 +6,38 @@
 #print("Rezultat: "\' \t \\ l\ni\nn\ne')
 # abs() reda opusul numarului introdus
 # pow( )
+
+number = 5 #int
+word = "Rezultat: " #string
+# boolean = True #bool
+#
+# print(word + boolean)
+# print(number)
+# del number
+# number = 7
+#
+# print(number)
+
+# digit = -4.23234 #float
+# str_num = '5' #string
+# #print(word + str(digit))
+# print(word + str(number + int(str_num)))
+#
+# del number
+
+
+num1 = int(input("Enter first number: "))
+
+num2 = int(input("Enter second number: "))
+
+print("Rezultat: ", num1 + num2)
+print("Rezultat: ", num1 - num2)
+print("Rezultat: ", num1 * num2)
+print("Rezultat: ", num1 / num2)
+print("Rezultat: ", num1 // num2)
+print("Rezultat: ", num1 ** num2)
+
+word = "Hello world "
+print(word * 10)
+
+
