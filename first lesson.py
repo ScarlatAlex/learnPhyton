1cc63dca6c47d61c236d670bf5cfef31d7c8a8ce
Alex <gitallex@mail.ru>
2022-11-24 12:51:11 +0200

    ad

--- first lesson.py
+++ first lesson.py
@@ -1,43 +1,68 @@
-# commnet
-# print("Rezultatul: ", max(3,4,6,4,7,9,12,3)) #min/max
-# print("Rezultat: ", 5 // 3)
-#print("Rezultat: ", round(5/3)
-#print("Rezultat: ", 7 , 15, sep="", end="1\n")
-#print("Rezultat: "\' \t \\ l\ni\nn\ne')
-# abs() reda opusul numarului introdus
-# pow( )
-
-number = 5 #int
-word = "Rezultat: " #string
-# boolean = True #bool
+# # commnet
+# # print("Rezultatul: ", max(3,4,6,4,7,9,12,3)) #min/max
+# # print("Rezultat: ", 5 // 3)
+# #print("Rezultat: ", round(5/3)
+# #print("Rezultat: ", 7 , 15, sep="", end="1\n")
+# #print("Rezultat: "\' \t \\ l\ni\nn\ne')
+# # abs() reda opusul numarului introdus
+# # pow( )
 #
-# print(word + boolean)
-# print(number)
-# del number
-# number = 7
+# number = 5 #int
+# word = "Rezultat: " #string
+# # boolean = True #bool
+# #
+# # print(word + boolean)
+# # print(number)
+# # del number
+# # number = 7
+# #
+# # print(number)
 #
-# print(number)
-
-# digit = -4.23234 #float
-# str_num = '5' #string
-# #print(word + str(digit))
-# print(word + str(number + int(str_num)))
+# # digit = -4.23234 #float
+# # str_num = '5' #string
+# # #print(word + str(digit))
+# # print(word + str(number + int(str_num)))
+# #
+# # del number
 #
-# del number
+#
+# num1 = int(input("Enter first number: "))
+#
+# num2 = int(input("Enter second number: "))
+#
+# print("Rezultat: ", num1 + num2)
+# print("Rezultat: ", num1 - num2)
+# print("Rezultat: ", num1 * num2)
+# print("Rezultat: ", num1 / num2)
+# print("Rezultat: ", num1 // num2)
+# print("Rezultat: ", num1 ** num2)
+#
+# word = "Hello world "
+# print(word * 10)
+#
+# # userData = int(input("Enter number: "))
+#
+# # isHappy = True
+# # if isHappy or userData == 6:
+# #     print("User is happy ")
+# # elif userData == 5:
+# #     print("Number is 5 ")
+# # elif userData == 7:
+# #     print("Number is 7")
+# # else:
+# #     print("User is unhappy ")
+#
+# # if userData != 5:
+# #     print("Suntem pe loc")
+# #     if userData > 6:
+# #         print("Number is bigger than 5 ")
 
+data = input()
 
-num1 = int(input("Enter first number: "))
+number = 5 if data == "Five" else 0
+if data == "FIve":
+    number = 5
+else:
+    number = 0
 
-num2 = int(input("Enter second number: "))
-
-print("Rezultat: ", num1 + num2)
-print("Rezultat: ", num1 - num2)
-print("Rezultat: ", num1 * num2)
-print("Rezultat: ", num1 / num2)
-print("Rezultat: ", num1 // num2)
-print("Rezultat: ", num1 ** num2)
-
-word = "Hello world "
-print(word * 10)
-
-
+print(number)
