706ae38dafd81bdab05f821ea70e06a48f94ea1b
Alex <gitallex@mail.ru>
2022-11-28 01:39:28 +0200

    add

--- cycles and operators.py
+++ cycles and operators.py
@@ -1,2 +1,55 @@
-for i in range(1, 6):
-    print(i)
+# Ciclurile este un cod special care iti perminte sa indeplinesti acelas cod de mai multe ori la rand,
+# in phyton exista ciclurile:
+# for
+# for i in range(6):
+#   print(i)
+
+# count = 0
+# word = "Hello world!"
+# for i in word:
+#     if i == "l":
+#         count += 1
+# print("Count: ", count)
+
+# while
+
+# i = 5
+#
+# while i <= 15:
+#     print(i)
+#     i += 1
+
+# isHasCar = True
+#
+# while isHasCar:
+#     if input("Enter data: ") == "Stop":
+#         isHasCar = False
+
+# for i in range(1, 11):
+#     if i == 5:
+#         break
+#
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# cautarea unui simbol
+#1
+# found = None
+# for i in "hello":
+#     if i == "l":
+#         found = True
+#         break
+# else:
+#     found = False
+# print(found)
+
+#2
+name = None
+for i in "Hello my big and beautifoul world":
+    if i == "l":
+        name = True
+        break
+else:
+    name = False
+print(name)
