fc4f64a4a24b5e92c229dde7c17d12cd06a736f6
Alex <gitallex@mail.ru>
2022-11-29 00:58:59 +0200

    add

--- list and function.py
+++ list and function.py
@@ -5,7 +5,13 @@ b = [2, 4, 7, 8, 5]
 # functia append -adauga numarul la sfarsiut sirului
 #         insert adauga un numar ce nu este in sirul indicat
 #         extend adauga sirul introdus la sfarsitul sirului initial
-#         sort - sortare
+#         sort - sortare numerele in crestere
+#         reverse - reverseaza ordinea numerelor
+#         pop - sterge ultimul element din lista dupa index
+#         remove(numar) - sterge numarul la indicarea acestuia
+#         clear - sterge toata lista
+#         count - numara elemente de acelas tip din lista
+#         len - scrie numarul de elemente introduse
 numbers = [5, 2, 7]
 # numbers[3] = 100
 numbers.append(100)
@@ -14,6 +20,10 @@ numbers.extend([2, 5, 7, ])
 # sau
 numbers.extend(b)
 numbers.sort()
-numbers.reverse()
+# numbers.reverse()
+# numbers.pop()
+# numbers.remove(2)
+# numbers.clear()
+# print(numbers.count(2))
 
-print(numbers)
+print(len(numbers))
