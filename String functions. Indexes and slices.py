f1c00c5e81fc6a526ec2d93b9a31580e812416c8
Alex <gitallex@mail.ru>
2022-11-29 11:33:15 +0200

    add

---
+++ String functions. Indexes and slices.py
@@ -0,0 +1,26 @@
+# fiecare string este o lista unde fiecare element este un simbol din string
+
+# word = "Soccer, basket, skate"
+
+# print(word.count('!')
+# print(word.isupper())
+# print(word.find('x'))
+# hobby = word.split(',')
+# print(hobby[0]
+
+# hobby = word.split(', ')
+#
+# for i in range(len(hobby)):
+#     hobby[i] = hobby[i].capitalize()
+#
+# result = " | ".join(hobby)
+# print(result)
+
+# indexurile
+# slices e adresarea la mai multe indexuri
+# word = 'soccer'
+# print(word[1:-1:1])
+
+lis = [6, 4, "lista", True, 6.2]
+print(lis[2:5:2])
+print(lis[::-2])
