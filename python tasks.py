ef0fe0e99d0c06ab55c47e9ace0eef93781124c7
Alex <gitallex@mail.ru>
2022-12-08 14:39:22 +0200

    Create python tasks.py

---
+++ python tasks.py
@@ -0,0 +1,12 @@
+# # There is a list
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#
+# # Print all elements less than 5
+# for el in a:
+#     if el < 5:
+#         print(el)
+
+# ---------------------------------------------
+
+
+
